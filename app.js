e00945ab3020956c035354b633ef6704fbb85849
feruzboboqulov02 <feruzboboqulov002@gmail.com>
2025-04-25 01:37:23 +0500

    commit_2

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 const express = require("express")
 
 const app =express()
+app.use(express.json())
+
+
+
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Hello brooo"})
+})
+
+app.post("/",(req,res)=>{
+    const {firstname,lastname} = req.body
+    const message = `Hello ${firstname} ${lastname}`
+    res.send(message)
+})
+
+app.delete("/:id",(req,res)=>{
+    const {id} = req.params
+    res.send(id)
+})
+
+app.put("/:id",( req,res)=>{
+    const {id} = req.params
+    const body = req.body
+
+    res.json({id,body}) 
+})
 
 const PORT = 8080
 app.listen(PORT,()=>{console.log(`Listening on - http://localhost:${PORT}`)})
